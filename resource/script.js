a312f97e61e7ae443fb8b49be3250ff104a09a99
ArviRA <arvindhak47@gmail.com>
2021-07-18 20:33:35 +0530

    validation done

--- resource/script.js
+++ resource/script.js
@@ -1,25 +1,59 @@
+var status = true;
 function validate()
 {   var gender;
+    var send_dict = {}
+    var Emp;
     var dob = document.getElementById("dob").value;
-    
     var fname = document.getElementById("fname").value;
+    var lname = document.getElementById("lname").value;
     var male = document.getElementById("g1").checked;
     var female = document.getElementById("g2").checked;
+    var employeStatus = document.getElementById("dropDown").value;
+    if(fname !="" && lname!="")
+    {
+        send_dict["fname"] = fname;
+        send_dict["lname"] = lname;
+    }
+    else
+    {
+        notVal("Name");
+    }
     if(male == false && female == false)
     {
-    
-        //alert("Fill all the fields")
+        notVal("Gender");
     }
     else if(male == true && female == false)
     {
-        //alert("male")
         gender = "male"
-
+        send_dict["gender"] = gender;
+    }
+    else
+    {
+        gender = "female"
+        send_dict["gender"] = gender;
+    }
+    if (dob =="")
+    {
+        notVal("DOB");
+    }
+    else
+    {
+        send_dict["dob"] = dob;
+    }
+    if (employeStatus =="")
+    {
+    notVal("Employed");
     }
     else{
-        //alert("female")
-        gender = "female"
+      send_dict["employed"] = employeStatus;  
     }
-
-    console.log(gender,dob);
+    if(status == true)
+    {
+    console.log(JSON.stringify(send_dict))
+    }
+}
+function notVal(data)
+{   status = false;
+    alert("All fields are mandatory\nFill the "+data+" field");
+    console.log(status)
 }
